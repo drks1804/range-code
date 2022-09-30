0b861699af03d40803777e8a8731e6ea634e45e5
drks1804 <113842527+drks1804@users.noreply.github.com>
2022-09-30 17:32:57 +0530

    Add files via upload

---
+++ range code.py
@@ -0,0 +1,15 @@
+list1 = [12,-7,5,64,-14]
+
+for i in list1:
+    if i>=0:
+        print(i)
+
+print("----------------")        
+             
+list2 = [12,14,-95,3]
+
+for i in list2:
+    if i>=0:
+        print(i)
+    
+    
